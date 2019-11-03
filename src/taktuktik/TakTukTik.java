f25bceee4bb812fcf3ea5697383252c443e4bff9
kpsdilshan <Supun@123>
2019-11-03 22:31:03 +0530

    adding the project into github

---
+++ src/taktuktik/TakTukTik.java
@@ -0,0 +1,14 @@
+
+package taktuktik;
+
+/**
+ *
+ * @author Nishan Chamith
+ */
+public class TakTukTik {
+
+   public static void main(String[] args) {
+      MainFrame m = new MainFrame();
+      m.setVisible(true);
+   }
+}
